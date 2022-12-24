66553f0a290a7fcf9428d5dd103b2ac642cc0ff2
Pawnak <kumarpawans@gmail.com>
2022-12-24 19:42:02 +0530

    Update MovieTicketControllerWithCircuitBrakerPattern.java

--- MovieTicketControllerWithCircuitBrakerPattern.java
+++ MovieTicketControllerWithCircuitBrakerPattern.java
@@ -27,3 +27,60 @@ public class MovieTicketControllerWithCircuitBrakerPattern {
         });
     }
 }
+
+class CircuitBreaker {
+    private static final int DEFAULT_THRESHOLD = 5;
+    private static final long DEFAULT_TIMEOUT = 1000L;
+    private int threshold;
+    private long timeout;
+    private int failureCount;
+    private long lastFailure;
+
+    public CircuitBreaker() {
+        this(DEFAULT_THRESHOLD, DEFAULT_TIMEOUT);
+    }
+
+    public CircuitBreaker(int threshold, long timeout) {
+        this.threshold = threshold;
+        this.timeout = timeout;
+    }
+
+    public <T> T execute(Callable<T> operation) throws Exception {
+        // check if circuit is open
+        if (failureCount >= threshold && System.currentTimeMillis() - lastFailure < timeout) {
+            throw new CircuitBreakerOpenException("Circuit breaker is open");
+        }
+
+        try {
+            // execute operation
+            return operation.call();
+        } catch (Exception e) {
+            // increment failure count
+            failureCount++;
+            lastFailure = System.currentTimeMillis();
+
+            // throw original exception
+            throw e;
+        }
+    }
+}
+
+class TokenService {
+    private static final String TOKEN_PREFIX = "Bearer ";
+    private UserDatabase userDatabase;
+
+    public TokenService(UserDatabase userDatabase) {
+        this.userDatabase = userDatabase;
+    }
+
+    public User verifyAccessToken(String authorization) {
+        // extract token from authorization header
+        if (authorization == null || !authorization.startsWith(TOKEN_PREFIX)) {
+            return null;
+        }
+        String token = authorization.substring(TOKEN_PREFIX.length());
+
+        // verify token and return user
+        return userDatabase.getUserByToken(token);
+    }
+}
