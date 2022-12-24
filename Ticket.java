267d4466b0394c6c1fb3e7d69bd9a3493fed34d0
Pawnak <kumarpawans@gmail.com>
2022-12-24 18:21:58 +0530

    Create Ticket.java

---
+++ Ticket.java
@@ -0,0 +1,13 @@
+public class Ticket {
+    private String id;
+    private String movieId;
+    private String theaterId;
+    private String showtime;
+    private List<Seat> seats;
+    private String userId;
+    private double price;
+    private Date purchaseDate;
+    private boolean refunded;
+
+    // constructors, getters, and setters
+}
