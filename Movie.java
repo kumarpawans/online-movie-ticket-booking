77b34c785a6b85c4041401b381e1665921cb7083
Pawnak <kumarpawans@gmail.com>
2022-12-24 18:25:39 +0530

    Create Movie.java

---
+++ Movie.java
@@ -0,0 +1,12 @@
+public class Movie {
+    private String id;
+    private String title;
+    private String genre;
+    private String plot;
+    private String cast;
+    private double rating;
+    private int duration;
+    private String posterUrl;
+
+    // constructors, getters, and setters
+}
