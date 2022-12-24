97ee5383cdb83552d04d661c3967448062478ab8
Pawnak <kumarpawans@gmail.com>
2022-12-24 19:52:22 +0530

    Update BookMovieTicketService.java

--- BookMovieTicketService.java
+++ BookMovieTicketService.java
@@ -3,6 +3,11 @@ public class BookMovieTicketService {
     private TheaterDatabase theaterDatabase;
     private TicketDatabase ticketDatabase;
     private PaymentService paymentService;
+    
+    private static final int AFTERNOON_SHOW_START_HOUR = 12;
+    private static final double AFTERNOON_SHOW_DISCOUNT = 0.2;
+    private static final double THIRD_TICKET_DISCOUNT = 0.5;
+
 
     public BookMovieTicketService(MovieDatabase movieDatabase, TheaterDatabase theaterDatabase, TicketDatabase ticketDatabase, PaymentService paymentService) {
         this.movieDatabase = movieDatabase;
@@ -20,10 +25,26 @@ public class BookMovieTicketService {
         // retrieve movie and theater details
         Movie movie = movieDatabase.getMovieById(movieId);
         Theater theater = theaterDatabase.getTheaterById(theaterId);
-
+        
         // calculate ticket price
-        double ticketPrice = movie.getPrice() * numTickets;
+        double ticketPrice = movie.getTicketPrice();
+        LocalDateTime showtimeDateTime = LocalDateTime.parse(showtime);
+        if (showtimeDateTime.getHour() >= AFTERNOON_SHOW_START_HOUR) {
+            ticketPrice *= (1.0 - AFTERNOON_SHOW_DISCOUNT);
+        }
+        if (numTickets >= 3) {
+            ticketPrice *= (1.0 - THIRD_TICKET_DISCOUNT);
+        }
+        
+        double totalPrice = ticketPrice * numTickets;
 
+        // apply discount if showtime is in the afternoon
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime showtimeDateTime = LocalDateTime.parse(showtime, formatter);
+        if (showtimeDateTime.getHour() >= 12) {
+            ticketPrice *= 0.8;
+        }
+        
         // process payment
         boolean paymentSuccessful = paymentService.processPayment(paymentDetails, ticketPrice);
         if (!paymentSuccessful) {
