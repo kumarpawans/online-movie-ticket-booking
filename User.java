c9674982344d8ff3f91747e1915185eaa6982fe5
Pawnak <kumarpawans@gmail.com>
2022-12-24 18:28:37 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,10 @@
+public class User {
+    private String id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String phone;
+
+    // constructors, getters, and setters
+}
