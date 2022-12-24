fca362053ed6ea6e691998270e14d9e9b250cd5c
Pawnak <kumarpawans@gmail.com>
2022-12-24 18:30:30 +0530

    Create Showtime.java

---
+++ Showtime.java
@@ -0,0 +1,9 @@
+public class Showtime {
+    private String movieId;
+    private String theaterId;
+    private Date startTime;
+    private Date endTime;
+    private List<Seat> seats;
+
+    // constructors, getters, and setters
+}
