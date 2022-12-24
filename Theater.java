3a5eea31d575060cac090598a1b81ee5846a1fb4
Pawnak <kumarpawans@gmail.com>
2022-12-24 18:27:13 +0530

    Create Theater.java

---
+++ Theater.java
@@ -0,0 +1,9 @@
+public class Theater {
+    private String id;
+    private String name;
+    private String location;
+    private int numRows;
+    private int numColumns;
+
+    // constructors, getters, and setters
+}
